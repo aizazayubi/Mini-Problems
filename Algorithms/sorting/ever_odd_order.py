e537acd4942b3427c848f7961bec5fd8bde44148
unknown <aizazayubi777@gmail.com>
2025-12-02 09:35:33 +0500

    even odd order

--- Algorithms/sorting/ever_odd_order.py
+++ Algorithms/sorting/ever_odd_order.py
@@ -1,8 +1,23 @@
-def custom_sort(arr):
-    evens = sorted([x for x in arr if x % 2 == 0])
-    odds = sorted([x for x in arr if x % 2 != 0], reverse=True)
+# def custom_sort(arr):
+#     evens = sorted([x for x in arr if x % 2 == 0])
+#     odds = sorted([x for x in arr if x % 2 != 0], reverse=True)
+#     return evens + odds
+#
+# # Example
+
+
+# print(custom_sort(arr))
+#
+# #another solution
+def custom_sort_linear(arr):
+    evens = []
+    odds = []
+    for x in arr:
+        if x % 2 == 0:
+            evens.append(x)
+        else:
+            odds.append(x)
     return evens + odds
 
-# Example
 arr = [7, 2, 9, 4, 3, 8]
-print(custom_sort(arr))
+print(custom_sort_linear(arr))
