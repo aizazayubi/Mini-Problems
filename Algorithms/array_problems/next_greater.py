8f77ef70e79b85ae147e1a982da27db184d20afa
unknown <aizazayubi777@gmail.com>
2025-12-03 11:11:06 +0500

    greater max with brute force

--- Algorithms/array_problems/next_greater.py
+++ Algorithms/array_problems/next_greater.py
@@ -1,20 +1,41 @@
-def next_greater_elements(arr):
+# def next_greater_elements(arr):
+#     n = len(arr)
+#     result = [-1] * n
+#     stack = []  # will store indices
+#
+#     for i in range(n - 1, -1, -1):
+#         while stack and arr[stack[-1]] <= arr[i]:
+#             stack.pop()
+#
+#         if stack:
+#             result[i] = arr[stack[-1]]
+#
+#         stack.append(i)
+#
+#     return result
+#
+#
+# # Example
+# arr = [4, 5, 2, 25]
+# print(next_greater_elements(arr))  # [5, 25, 25, -1]
+#----------------------------------------------------------------
+#---------BRUTE FORCE --------------------------------------
+#----------------------------------------------------------------
+def next_greater(arr):
     n = len(arr)
-    result = [-1] * n
-    stack = []  # will store indices
+    result = []
 
-    for i in range(n - 1, -1, -1):
-        while stack and arr[stack[-1]] <= arr[i]:
-            stack.pop()
-
-        if stack:
-            result[i] = arr[stack[-1]]
-
-        stack.append(i)
+    for i in range(n):
+        next_big = -1
+        for j in range(i+1, n):
+            if arr[j] > arr[i]:
+                next_big = arr[j]
+                break
+        result.append(next_big)
 
     return result
 
 
 # Example
 arr = [4, 5, 2, 25]
-print(next_greater_elements(arr))  # [5, 25, 25, -1]
+print(next_greater(arr))  # [5, 25, 25, -1]
