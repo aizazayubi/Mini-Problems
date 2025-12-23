58f95b13c7f9a3ba6ae8b9c509a22fa6966ccc9a
Aizaz Ali Shah <46074258+aizazayubi@users.noreply.github.com>
2025-12-23 09:41:04 +0500

    Implement is_sorted function for array checking
    Added a function to check if an array is sorted.

--- Algorithms/array_problems/sorted_check.py
+++ Algorithms/array_problems/sorted_check.py
@@ -1,1 +1,11 @@
+def is_sorted(arr):
+    for i in range(len(arr) - 1):
+        if arr[i] > arr[i + 1]:
+            return False
+    return True
+
+
+# Examples
+print(is_sorted([1, 3, 5, 7]))
+print(is_sorted([1, 5, 3, 7]))
 
