0a1db37a218aacaf68984bfea2ef3599adb88d35
Aizaz Ali Shah <46074258+aizazayubi@users.noreply.github.com>
2025-12-23 09:38:05 +0500

    Refactor second_largest function implementation
    Refactor second_largest function for clarity and efficiency.

--- Algorithms/array_problems/secondLargest.py
+++ Algorithms/array_problems/secondLargest.py
@@ -1,20 +1,12 @@
 def second_largest(arr):
-    if len(arr) < 2:
-        return None
-
-    largest = second = float('-inf')
+    first = second = float('-inf')
 
     for x in arr:
-        if x > largest:
-            second = largest
-            largest = x
-        elif x != largest and x > second:
+        if x > first:
+            second = first
+            first = x
+        elif first > x > second:
             second = x
 
-    return second
-
-
-# Example
-arr = [10, 5, 20, 8]
-print(second_largest(arr))  # 10
+    return second if second != float('-inf') else None
 
