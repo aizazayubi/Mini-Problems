a6003f3b62d38645d163bcf4b55a3c896244f3dd
Aizaz Ali Shah <46074258+aizazayubi@users.noreply.github.com>
2025-12-24 18:17:26 +0500

    Update missing_number_xor.py

--- Algorithms/array_problems/missing_number_xor.py
+++ Algorithms/array_problems/missing_number_xor.py
@@ -1,1 +1,17 @@
+def missing_number_xor(arr):
+    n = len(arr) + 1
+    xor_all = 0
+
+    for i in range(1, n + 1):
+        xor_all ^= i
+
+    for x in arr:
+        xor_all ^= x
+
+    return xor_all
+
+
+
+arr = [1, 2, 4, 5]
+print(missing_number_xor(arr))  # 3
 
